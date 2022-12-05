10c766ee70fbd5bc316a7c3a71dd41790e008cfb
unknown <bastrakov.sergei@gmail.com>
2022-12-05 07:31:23 +0300

    Add day 3-4 solutions

---
+++ AOCCommon/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AOCCommon;
+
+public static class Extensions
+{
+    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T>? items, int maxItems)
+        => (items ?? Enumerable.Empty<T>())
+            .Select((item, inx) => new { item, inx, })
+            .GroupBy(x => x.inx / maxItems)
+            .Select(g => g.Select(x => x.item));
+}
---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,60 @@
+﻿var file = await File.ReadAllLinesAsync("input.txt");
+
+var solution1 = Solution1Solve();
+var solution2 = Solution2Solve();
+
+Console.WriteLine($"Solution 1: {solution1}");
+Console.WriteLine($"Solution 2: {solution2}");
+
+int Solution1Solve()
+{
+    var count = 0;
+    foreach (var line in file)
+    {
+        var pairs = line.Split(",");
+        var first = ParseRange(pairs[0]);
+        var second = ParseRange(pairs[1]);
+
+        if (first.Contains(second) || second.Contains(first))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int Solution2Solve()
+{
+    var count = 0;
+    foreach (var line in file)
+    {
+        var pairs = line.Split(",");
+        var first = ParseRange(pairs[0]);
+        var second = ParseRange(pairs[1]);
+
+        if (first.Overlaps(second))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+Range ParseRange(string input)
+{
+    var parts = input.Split("-");
+    return new Range(int.Parse(parts[0]), int.Parse(parts[1]));
+}
+
+public record Range(int Left, int Right)
+{
+    public bool Contains(Range other)
+        => Left <= other.Left && Right >= other.Right;
+
+    public bool Overlaps(Range other) 
+        => other.Left <= Right && Left <= other.Right || 
+           Left <= other.Right && other.Left <= Right;
+}
